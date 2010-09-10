4e822699e23a8ee2e7868d3cf9c703807aa9f86f
Lucas Uyezu <lucas.uyezu@gmail.com>
2010-09-10 12:13:56 -0300

    Added Devise's bcrypt support

---
+++ lib/casserver/authenticators/sql_devise.rb
@@ -0,0 +1,66 @@
+require 'casserver/authenticators/sql'
+
+require File.dirname(__FILE__) + '/devise_encryptors/bcrypt'
+
+begin
+  require 'active_record'
+rescue LoadError
+  require 'rubygems'
+  require 'active_record'
+end
+
+# This is a version of the SQL authenticator that works with Devise configured to use Bcrypt.
+# Before using this, you MUST configure Devise to use Bcrypt and set the same values for config.stretches and config.pepper in config/initializers/devise.rb.
+# config.
+#
+# If you need more info on how to use Devise:
+#
+# * git://github.com/plataformatec/devise.git
+#
+# Usage:
+
+# authenticator:
+#   class: CASServer::Authenticators::SQLDevise
+#   database:
+#     adapter: mysql
+#     database: some_database_with_users_table
+#     user: root
+#     password:
+#     server: localhost
+#   user_table: users
+#   username_column: username
+#   password_column: encrypted_password
+#   salt_column: password_salt
+#   encryptor: Bcrypt
+#   stretches: (same as config.stretches in config/initializers/devise.rb)
+#   pepper:    (same as config.pepper    in config/initializers/devise.rb)
+#
+class CASServer::Authenticators::SQLDevise < CASServer::Authenticators::SQL
+
+  def validate(credentials)
+    read_standard_credentials(credentials)
+    raise_if_not_configured
+
+    user_model = self.class.user_model
+
+    username_column = @options[:username_column] || "username"
+    password_column = @options[:password_column] || "encrypted_password"
+    salt_column     = @options[:salt_column] || "password_salt"
+
+    $LOG.debug "#{self.class}: [#{user_model}] " + "Connection pool size: #{user_model.connection_pool.instance_variable_get(:@checked_out).length}/#{user_model.connection_pool.instance_variable_get(:@connections).length}"
+    results = user_model.find(:all, :conditions => ["#{username_column} = ?", @username])
+    user_model.connection_pool.checkin(user_model.connection)
+
+    encryptor = Devise::Encryptors::Bcrypt
+
+    if results.size > 0
+      $LOG.warn("Multiple matches found for user '#{@username}'") if results.size > 1
+      user = results.first
+
+      return encryptor.digest(@password, @options[:stretches], user.send(salt_column), @options[:pepper])
+    else
+      return false
+    end
+  end
+end
+
